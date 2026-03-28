9d4f9b1ef0476fc22247867db7434cc00793acc8
Aarya Bhatt <bhattaarya1729@gmail.com>
2026-03-28 07:42:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const multer = require('multer');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const storage = multer.diskStorage({
   destination: 'uploads/',
